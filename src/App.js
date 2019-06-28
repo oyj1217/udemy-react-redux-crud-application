ae72a1083c4aff19460be5e77680d2a676ea7f5c
kure <kureshima@softstudio.co.jp>
2019-06-28 17:39:10 +0900

    create functional component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
 
-class App extends Component
+// class App extends Component
+// {
+//   render()
+//   {
+//     const greeting = "HI!Tom";
+//     const dom = <h1 className="foo">{greeting}</h1>
+//     return dom;
+//   }
+// }
+
+const App = () =>
 {
-  render()
-  {
-    const greeting = "HI!Tom";
-    const dom = <h1 className="foo">{greeting}</h1>
-    return dom;
-  }
+  return (
+    <React.Fragment>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </React.Fragment>
+  )
+}
+
+const Cat = () =>
+{
+  return <div>mei</div>
 }
 
 export default App;
